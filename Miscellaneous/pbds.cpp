8e30b0f288519de231dada99cf61bccc4f272042
Kundan Kumar <63704722+kundan9170@users.noreply.github.com>
2024-07-21 19:19:55 +0530

    Create pbds.cpp

---
+++ Miscellaneous/pbds.cpp
@@ -0,0 +1,3 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ordered_set tree<int, null_type,less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
